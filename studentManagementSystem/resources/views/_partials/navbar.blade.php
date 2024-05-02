f01a875603f75d452e902e7cc0556a71d2505d98
Malshani RathnaSri <malshanirathnasri@gmail.com>
2024-05-02 23:07:13 +0530

    #1 Feat: Create Layout Section

---
+++ studentManagementSystem/resources/views/_partials/navbar.blade.php
@@ -0,0 +1,23 @@
+<div class="">
+    <div class="flex justify-between">
+        <div>
+            <img src="/logo.jpg" alt="logo" width="100" height="100">
+        </div>
+        <div class="flex justify-center space-x-10 py-2.5">
+            <div>hhhhhh</div>
+            <div>hhhhhh</div>
+            <div>hhhhhh</div>
+            <div>hhhhhh</div>
+            <div>hhhhhh</div>
+
+        </div>
+        <div class="flex justify-center space-x-3">
+            <div>
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Login</button>
+            </div>
+            <div>
+                <a href="{{ route('registration') }}" type="button"  class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Registration</a>
+            </div>
+        </div>
+    </div>
+</div>
--- studentManagementSystem/routes/web.php
+++ studentManagementSystem/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Registration;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout');
 });
+
+Route::get('/registration',[Registration::class,'index'])->name('registration');
